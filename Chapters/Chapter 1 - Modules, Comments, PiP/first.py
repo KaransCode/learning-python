285feaa2605a91262602e77bb46f6c7a6c28e21f
Karan Singh <karan.ts265@gmail.com>
2025-09-16 23:41:47 +0530

    first
    starting with python

---
+++ Chapters/Chapter 1 - Modules, Comments, PiP/first.py
@@ -0,0 +1,10 @@
+# Author - Karan Singh
+# This is my first program
+
+# Single Line Comment
+print("Hello! Python World")
+'''
+This is use of multi-line comments 
+in Python.
+'''
+print("This is the last line of this program.")
---
+++ Chapters/Chapter 1 - Modules, Comments, PiP/module.py
@@ -0,0 +1,5 @@
+# Module imports in Python
+
+# import <module-name>
+import numpy as np
+import pandas as pd
